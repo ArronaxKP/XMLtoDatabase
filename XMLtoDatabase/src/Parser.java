3d8a4451f7ad7ff89837080c4192fede90499450
killerwizz1 <killerwizz1@LENOVO>
2013-12-01 20:11:10 +0000

    Parsing 1000 XML in 16s or 49s. Need to correctly identify each entry

--- XMLtoDatabase/src/Parser.java
+++ XMLtoDatabase/src/Parser.java
@@ -13,6 +13,7 @@ public class Parser {
 	private VTDGen vg;
 	private VTDNav vn;
 	private String ROOT = "";
+	private int i = 0;
 
 	public Parser(String XML) {
 		try {
@@ -26,26 +27,54 @@ public class Parser {
 			System.err.println("UTF-8 is the only encoding supported currently.");
 		}
 	}
+	
+	
+	public String getValue(String xpath){
+		String result = null;
+	    try {
+	    	vn.toElement(VTDNav.ROOT);
+	    	
+		    AutoPilot ap = new AutoPilot();
+		    ap.selectXPath(this.ROOT);
+		    
+		    AutoPilot ap2 = new AutoPilot();
+			ap2.selectXPath(xpath);
+			
+	        ap.bind(vn);
+	        ap2.bind(vn);
+	        //XPath eval returns one node at a time
+	        while (ap.evalXPath() != -1)
+	        {
+	        //ap.evalXPath();
+	        	i++;
+	            vn.push();
+	            result = ap2.evalXPathToString();
+	            System.out.println(ROOT);
+	            System.out.println(xpath);
+	            System.out.println(result);
+	            // doesn't forget this since the next iteration will reuse
+	            // ap2's XPath!!! 
+	            ap2.resetXPath();
+	            vn.pop();
+	        }
+	        ap.resetXPath();
+        } catch (XPathParseException | XPathEvalException | NavException e) {
+			System.err.println("Error with the xpath parsing: "+xpath);
+			e.printStackTrace();
+		}
+	    return result;
+	}
 
-	public String getValue(String xpath) {
+	public String getValueFromRoot(String xpath) {
 		String result = null;
 		try {
-			if(ROOT != null){
-				AutoPilot rootPilot = new AutoPilot();
-				rootPilot.selectXPath(xpath);
-				rootPilot.bind(vn);
-				vn.toElement(rootPilot.evalXPath());
-				rootPilot.resetXPath();
-			} else {
-				vn.toElement(VTDNav.ROOT);
-			}
-			
+			vn.toElement(VTDNav.ROOT);
 			AutoPilot pilot = new AutoPilot();
 			pilot.selectXPath(xpath);
 			pilot.bind(vn);
 			result = pilot.evalXPathToString();
 			pilot.resetXPath();
-		} catch (XPathParseException | NavException | XPathEvalException e) {
+		} catch (XPathParseException | NavException e) {
 			System.err.println("Error with the xpath parsing: "+xpath);
 			e.printStackTrace();
 		}
